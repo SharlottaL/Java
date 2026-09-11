79a152ea909428c1eda6bb4574e81b4d70ceabbd
SharlottaL <botanichkagigantika@gmail.com>
2026-09-11 12:22:56 +0300

    Five DONE

--- src/Five.java
+++ src/Five.java
@@ -2,27 +2,27 @@ import java.util.Scanner;
 public class Five {
     void main() {
         //1).
-//        Scanner console = new Scanner(System.in);
-//        System.out.println("Введите делимое:");
-//        int dividend = console.nextInt();
-//        System.out.println("Введите делитель:");
-//        int divisor = console.nextInt();
-//        try {
-//            int quotient = dividend / divisor;
-//            System.out.println("Частное:" + quotient);
-//        } catch (ArithmeticException e) {
-//            System.out.println("На ноль делить нельзя");
-//        } finally {
-//            console.close();
-//        }
-        //2).
         Scanner console = new Scanner(System.in);
+        System.out.println("Введите делимое:");
+        int dividend = console.nextInt();
+        System.out.println("Введите делитель:");
+        int divisor = console.nextInt();
+        try {
+            int quotient = dividend / divisor;
+            System.out.println("Частное:" + quotient);
+        } catch (ArithmeticException e) {
+            System.out.println("На ноль делить нельзя");
+        } finally {
+            console.close();
+        }
+       // 2).
+        Scanner consoleP = new Scanner(System.in);
         boolean num = false;
         int number = 0;
         while (!num)
         {
             System.out.println("Введите строку для дальнейшего преобразования:");
-            String string = console.nextLine();
+            String string = consoleP.nextLine();
         try {
             number = Integer.parseInt(string);
             num = true;
@@ -31,6 +31,31 @@ public class Five {
         }
         }
         System.out.println("Строка из чисел преобразована успешно:" + number);
-        console.close();
+        consoleP.close();
+       // 3)
+        Scanner consoleAr = new Scanner(System.in);
+        int[] array = {2,18,25,69,12};
+
+        System.out.println("Введите индекс массива:");
+        int index = consoleAr.nextInt();
+        System.out.println("Введите делитель массива:");
+        int divisorAr = consoleAr.nextInt();
+        try {
+        int dividendAr = array[index];
+        double quotient = (double)dividendAr / divisorAr;
+            System.out.println("Результат деления:"+quotient);
+        }
+        catch (ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("Введено значение выходящее за пределы массива");
+        }
+        catch (ArithmeticException e)
+        {
+            System.out.println("На ноль делить нельзя");
+        }
+        finally {
+            System.out.println("Попытка обработки завершена");
+            consoleAr.close();
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-
+    //1)
+ Book bookF = new Book("Martin Eden", 448);
+ Book bookS = new Book("Towards Zero", 242);
+    System.out.println("Книга:" + bookF.title + " страниц:" + bookF.pages + '\n');
+    System.out.println("Книга:" + bookS.title + " страниц:" + bookS.pages + '\n');
+   //2)
+    Rectangle restangleF = new Rectangle(10.5, 2.4);
+    Rectangle restangleS = new Rectangle(5.2, 8.7);
+    System.out.println("Площадь:" + "restangleF:" + restangleF.getArea() + "restangleS" + restangleS.getArea());
+    System.out.println("Периметр:" + "restangleF:" + restangleF.getPerimeter() + "restangleS" + restangleS.getPerimeter());
+    if(restangleF.getArea() > restangleS.getArea())
+    {
+        System.out.println("У прямоугольника со сторонами:" + restangleF.width + " и " + restangleF.height + " площадь больше");
+    }
+    else if(restangleF.getArea() == restangleS.getArea())
+    {
+        System.out.println("У прямоугольникov площадь одинаковая");
+    }
+    else
+    {
+        System.out.println("У прямоугольника со сторонами:" + restangleS.width + " и " + restangleS.height + " площадь больше");
+    }
 }
